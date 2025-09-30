28a8a6c551a0c2bc683299fcff5e2a3191a38cf2
Kim, Hyeonseo <dodok8@gmail.com>
2025-10-01 02:43:11 +0900

    연습문제 1-2

--- chapter 1/browser.py
+++ chapter 1/browser.py
@@ -5,57 +5,67 @@ import ssl
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
-        if "/" not in url:
-            url = url + "/"
-        self.host, url = url.split("/", 1)
-        self.path = "/" + url
+        assert self.scheme in ["http", "https", "file"]
 
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
+        if self.scheme == "file":
+            self.host = ""
+            self.path = url
+            self.port = None
+        else:
+            if "/" not in url:
+                url = url + "/"
+            self.host, url = url.split("/", 1)
+            self.path = "/" + url
 
-        if ":" in self.host:
-            self.host, port = self.host.split(":", 1)
-            self.port = int(port)
+            if self.scheme == "http":
+                self.port = 80
+            elif self.scheme == "https":
+                self.port = 443
+
+            if ":" in self.host:
+                self.host, port = self.host.split(":", 1)
+                self.port = int(port)
 
     def request(self, headers=dict()):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-        )
-        s.connect((self.host, self.port))
+        if self.scheme != "file":
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+            )
+            s.connect((self.host, self.port))
 
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        request = "GET {} HTTP/1.1\r\n".format(self.path)
-        request += "Host: {}\r\n".format(self.host)
-        request += "Connection: close\r\n"
-        for key in headers:
-            request += "{}: {}\r\n".format(key, headers[key])
-        request += "\r\n"
-        s.send(request.encode("utf8"))
+            request = "GET {} HTTP/1.1\r\n".format(self.path)
+            request += "Host: {}\r\n".format(self.host)
+            request += "Connection: close\r\n"
+            for key in headers:
+                request += "{}: {}\r\n".format(key, headers[key])
+            request += "\r\n"
+            s.send(request.encode("utf8"))
 
-        response = s.makefile("r", encoding="utf8", newline="" "\r\n")
+            response = s.makefile("r", encoding="utf8", newline="" "\r\n")
 
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
+            statusline = response.readline()
+            version, status, explanation = statusline.split(" ", 2)
 
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == "\r\n":
-                break
-            header, value = line.split(":", 1)
-            response_headers[header.casefold()] = value.strip()
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
+            response_headers = {}
+            while True:
+                line = response.readline()
+                if line == "\r\n":
+                    break
+                header, value = line.split(":", 1)
+                response_headers[header.casefold()] = value.strip()
+            assert "transfer-encoding" not in response_headers
+            assert "content-encoding" not in response_headers
 
-        body = response.read()
-        s.close()
+            body = response.read()
+            s.close()
+        else:
+            with open(self.path, "r") as file:
+                body = file.read()
 
         return body
 
@@ -79,4 +89,20 @@ def load(url):
 if __name__ == "__main__":
     import sys
 
-    load(URL(sys.argv[1]))
+    if len(sys.argv) > 1:
+        load(URL(" ".join(sys.argv[1:])))
+    else:
+        try:
+            load(
+                URL(
+                    "file:///Users/dodok/repos/web-browser-engineering/"
+                    "chapter 1/blank.html"
+                )
+            )
+        except FileNotFoundError:
+            load(
+                URL(
+                    "file:///home/dodok8/Development/web-browser-engineering/"
+                    "chapter 1/blank.html"
+                )
+            )

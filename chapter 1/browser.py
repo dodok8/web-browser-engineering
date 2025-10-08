774df39fcf2fb39174e8624003f9ceaacc061c06
Hyeonseo Kim <dodok8@gmail.com>
2025-10-08 18:30:56 +0900

    연습문제 1-6

--- chapter 1/browser.py
+++ chapter 1/browser.py
@@ -3,6 +3,8 @@ import ssl
 
 
 class URL:
+    sockets = {}
+
     def __init__(self, url):
         self.view_source = False
 
@@ -41,41 +43,64 @@ class URL:
         if self.scheme == "data":
             return self.data
         elif self.scheme in ["https", "http"]:
-            s = socket.socket(
-                family=socket.AF_INET,
-                type=socket.SOCK_STREAM,
-            )
-            s.connect((self.host, self.port))
+            cache_key = (self.scheme, self.host, self.port)
 
-            if self.scheme == "https":
-                ctx = ssl.create_default_context()
-                s = ctx.wrap_socket(s, server_hostname=self.host)
+            if cache_key in URL.sockets:
+                s = URL.sockets[cache_key]
+            else:
+                s = socket.socket(
+                    family=socket.AF_INET,
+                    type=socket.SOCK_STREAM,
+                )
+                s.connect((self.host, self.port))
+
+                if self.scheme == "https":
+                    ctx = ssl.create_default_context()
+                    s = ctx.wrap_socket(s, server_hostname=self.host)
+
+                URL.sockets[cache_key] = s
 
             request = "GET {} HTTP/1.1\r\n".format(self.path)
             request += "Host: {}\r\n".format(self.host)
-            request += "Connection: close\r\n"
+            request += "Connection: keep-alive\r\n"
             for key in headers:
                 request += "{}: {}\r\n".format(key, headers[key])
             request += "\r\n"
             s.send(request.encode("utf8"))
 
-            response = s.makefile("r", encoding="utf8", newline="" "\r\n")
+            response = s.makefile("rb", newline=b"\r\n")
 
-            statusline = response.readline()
+            statusline = response.readline().decode("utf8")
             version, status, explanation = statusline.split(" ", 2)
 
             response_headers = {}
             while True:
-                line = response.readline()
+                line = response.readline().decode("utf8")
                 if line == "\r\n":
                     break
                 header, value = line.split(":", 1)
                 response_headers[header.casefold()] = value.strip()
-            assert "transfer-encoding" not in response_headers
-            assert "content-encoding" not in response_headers
+                # 본문 길이가 주어져서 내려오므로, Transfer-Encoding은 필요 없다.
+                # 단 view-source의 경우, chunked 로 내려오므로 이에 대한 처리가 필요하다.
 
-            body = response.read()
-            s.close()
+            if (
+                "transfer-encoding" in response_headers
+                and response_headers["transfer-encoding"] == "chunked"
+            ):
+                # chunked 파싱
+                body = ""
+                while True:
+                    chunk_size_line = response.readline()
+                    chunk_size_line = chunk_size_line.decode("utf-8").strip()
+                    chunk_size = int(chunk_size_line, 16)  # hex to int
+                    if chunk_size == 0:
+                        break
+                    chunk_data = response.read(chunk_size).decode("utf-8")
+                    response.readline()  # \r\n 제거
+                    body += chunk_data
+            else:
+                content_length = int(response_headers["content-length"])
+                body = response.read(content_length).decode("utf-8")
         else:
             with open(self.path, "r") as file:
                 body = file.read()

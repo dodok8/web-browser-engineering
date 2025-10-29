772b727cf5105e7095092058f8372e4d5c16a1f8
Hyeonseo Kim <dodok8@gmail.com>
2025-10-29 15:37:51 +0900

    연습문제 2-2 스크롤 구현

--- soyorin/browser.py
+++ soyorin/browser.py
@@ -3,8 +3,9 @@ from soyorin.connection import Connection
 from soyorin.cache import FileCache, InMemoryCache
 from soyorin.url import URL
 from soyorin.lexer import Lexer
+
 import tkinter
-import tkinter.font
+import platform
 
 
 class Browser:
@@ -20,16 +21,37 @@ class Browser:
         self.canvas.pack()
         self.layout = Layout(Browser.WIDTH, Browser.HEIGHT, hstep=13, vstep=18)
 
-        self.scroll = 0
-        self.window.bind("<Down>", self.scrolldown)
-        self.window.bind("<Up>", self.scrollup)
+        self.scroll = 0.0
+        self.window.bind("<Down>", self.__scroll_down)
+        self.window.bind("<Up>", self.__scroll_up)
+        self.window.bind("<MouseWheel>", self.__scroll_wheel)
+        self.window.bind("<Button-4>", self.__scroll_wheel)
+        self.window.bind("<Button-5>", self.__scroll_wheel)
 
-    def scrolldown(self, e):
+    def __scroll_wheel(self, e: tkinter.Event):
+        if platform.system() == "Windows":
+            self.scroll -= Browser.SCROLL_STEP * (e.delta / 120)
+        elif platform.system() == "Darwin":
+            self.scroll -= Browser.SCROLL_STEP * e.delta
+        elif platform.system() == "Linux":
+            if e.num == 4:  # Scroll up
+                self.scroll -= Browser.SCROLL_STEP
+            elif e.num == 5:  # Scroll down
+                self.scroll += Browser.SCROLL_STEP
+
+        if self.scroll < 0:
+            self.scroll = 0
+
+        self.draw()
+
+    def __scroll_down(self, e):
         self.scroll += Browser.SCROLL_STEP
         self.draw()
 
-    def scrollup(self, e):
+    def __scroll_up(self, e):
         self.scroll -= Browser.SCROLL_STEP
+        if self.scroll < 0:
+            self.scroll = 0
         self.draw()
 
     def draw(self):
